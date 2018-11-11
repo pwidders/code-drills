87d89e35f6e64a1b81b9eb577dd0942c11f36196
Pete Widders <pwidders@hotmail.com>
2018-11-10 19:35:04 -0800

    Completed challenge 03-01

--- 03-Javascript/03-core-js-user-input-and-dom-1/challenge-prompt.js
+++ 03-Javascript/03-core-js-user-input-and-dom-1/challenge-prompt.js
@@ -3,29 +3,28 @@
 
 
 // Declare a variable `userName`
-
+var userName;
 
 // Prompt the user for their name and store their response into `userName`
 // hint: use javascript's built in `prompt` method
 
-
+userName = prompt("What is your name?")
 
 // Create an alert welcoming the user to the "Sandwich Shop" using the name they just gave us
-
-
+alert("Welcome to the Sandwich Shop, " + userName + "!");
 
 // Using the `confirm` method, ask the user if they would like a sandwich
 // Store their response into a new `wantsSandwich` variable
-
-
+var wantsSando = confirm('You want a sandwich');
 
 // Check if the user wanted a sandwich
-
-
+if (wantsSando === true) {
     // If the user did want a sandwich, use the `document.write` method to write "Your sandwich will be ready soon!" to the DOM
-
-    
-// Else, say goodbye to the user using the name they gave us earlier
-
+    document.write("Your sandwich will be ready soon!")
+}
+    // Else, say goodbye to the user using the name they gave us earlier
+    else {
+        alert("Goodbye " + userName);
+    }
 
 
